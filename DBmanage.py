22254719edb5c80e82c96d34f67b7b2de95d8735
gyudong <kelee0812.2@gmail.com>
2020-12-11 21:01:06 +0900

    modify DBmanage.py

--- DBmanage.py
+++ DBmanage.py
@@ -1,18 +1,18 @@
 import psycopg2
 
-id = 0
 
 def get_connection():
-    conn = psycopg2.connect(host='localhost', dbname='url', user='gyudong', password='pw', port='5432')
+    conn = psycopg2.connect(host='localhost', dbname='urlshorten', user='gyudong', password='pw', port='5432')
 
     return conn
+
 
 def find_url(oldurl):
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = 'select * from oldurl where url=(%s);'
-    cursor.execute(sql, oldurl)
+    sql = 'select * from oldurl where url=%s;'
+    cursor.execute(sql, [oldurl])
     row = cursor.fetchall()
 
     conn.commit()
@@ -20,27 +20,36 @@ def find_url(oldurl):
     cursor.close()
     conn.close()
 
-    return row
+    return row[0]
+
 
 def insert_url(oldurl):
+    try:
+        find_url(oldurl)
+
+        return
+
+    except:
+        pass
+
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = 'insert into oldurl (index, url) values(%s, %s);'
-    cursor.execute(sql, (id, oldurl))
+    sql = 'insert into oldurl(url) values(%s);'
+    cursor.execute(sql, [oldurl])
 
     conn.commit()
-    id += 1
 
     cursor.close()
     conn.close()
+
 
 def find_oldurl(index):
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = 'select * from oldurl where index=(%s);'
-    cursor.execute(sql, index)
+    sql = 'select * from oldurl where id=%s;'
+    cursor.execute(sql, [index])
     row = cursor.fetchall()
 
     conn.commit()
